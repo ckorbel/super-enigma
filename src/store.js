278a7f49578f00f5ab6445a03eb76b1963acb8df
Chris Korbel <christopher.j.korbel@jpl.nasa.gov>
2018-10-22 11:25:38 -0700

    crash course vuex

--- src/store.js
+++ src/store.js
@@ -5,13 +5,46 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    title: 'my title'
-
+    title: 'my title',
+    links: [
+      'http://google.com',
+      'http://espn.com',
+      'http://youtube.com'
+    ]
   },
+
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
+  },
+
   mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link);
+    }, 
 
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    },
+
+    REMOVE_ALL: (state) => {
+      state.links = [];
+    }
   },
-  actions: {
 
+  actions: {
+    removeLink: (context, link) => {
+      context.commit('REMOVE_LINK', link)
+    },
+
+    removeAll({commit}) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          .resolve()
+        }, 1500)
+      })
+    }
   }
 })
